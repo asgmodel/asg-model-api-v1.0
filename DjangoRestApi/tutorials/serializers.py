eca77f92790a96145317d965a37c0704f6b2dab7
asgmodel <asgmodel@gmail.com>
2024-04-13 15:48:49 +0000

    first commit

---
+++ DjangoRestApi/tutorials/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from tutorials.models import Tutorial,Scenario
+from django.urls import ModelPums
+import sys
+sys.path.insert(0, "/home/asgmodel/django-rest-api/DjangoRestApi/tutorials/ASG")
+
+
+from TypeModels import TypeModel
+from BasePath import BASEPATH
+from ModelTEC import *
+from ASGModels import ASG
+
+
+MST=TEC(typemodel="svmL",model_setting=TypeModel['technique'])
+MST.loadmodel()
+ASGAI=ASG()
+class TutorialSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tutorial
+        fields = ('id',
+                  'title',
+                  'description',
+                  'published')
+
+class ScenarioSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Scenario
+        fields = ('id',
+                  'seqtactic',
+                  'iduser',
+                  'seqtec','score')
+
+
+
